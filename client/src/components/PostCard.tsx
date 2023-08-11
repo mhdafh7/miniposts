4790825a390dd71ecf20bc9f267c07d70c8ce817
mhdafh7 <mhdafh@tuta.io>
2023-08-11 14:43:04 +0530

    options on card hover

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,6 +1,6 @@
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { AsyncThunkAction } from "@reduxjs/toolkit";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useDispatch } from "react-redux";
 import { deletePost } from "../features/posts/postServices";
 import { UserType } from "./PostForm";
@@ -31,25 +31,29 @@ const Post = ({
     AsyncThunkAction<string, string, any>
   >;
   const user: UserType = JSON.parse(localStorage.getItem("profile") as string);
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
-    <article className="card w-96 bg-neutral-content shadow-xl transition-all hover:shadow-2xl hover:bg-neutral-focus hover:text-base-100 cursor-pointer">
+    <article
+      className="card w-96 shadow-xl transition-all hover:shadow-2xl hover:bg-neutral-focus cursor-pointer border-2 border-current"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
-        <div className="card-actions justify-start items-center">
+        <div className="card-actions justify-start items-center relative">
           <p className="opacity-60">post by @{username}</p>
-          {user.result.username === username ||
-          user.result.username === "mhdafh" ? (
-            <>
+          {user.result.username === username && isHovering ? (
+            <span className="absolute bottom-1 right-0 flex gap-2">
               <button
-                className="btn btn-error btn-square transition-colors hover:bg-red-500"
+                className="btn btn-error btn-square transition-all hover:bg-red-500"
                 onClick={() => dispatch(deletePost(_id as string))}
               >
                 <TrashIcon className="w-5 h-5" />
               </button>
               <button
-                className="btn btn-info btn-square transition-colors hover:bg-cyan-500"
+                className="btn btn-info btn-square transition-all hover:bg-cyan-500"
                 onClick={() => {
                   setCurrentId(_id as string);
                   setModalOpen(true);
@@ -57,7 +61,7 @@ const Post = ({
               >
                 <PencilSquareIcon className="w-5 h-5" />
               </button>
-            </>
+            </span>
           ) : null}
         </div>
       </div>
